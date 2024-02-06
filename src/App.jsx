75c6e62ff20c156e22240e607c50c15dca4d6a48
zubairkhan94049 <zubairkhan94049@gmail.com>
2024-02-07 00:05:39 +0500

    part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
 import RQ1 from './components/RQ1/RQ1';
+import RQ2 from './components/RQ2/RQ2';
+
+
+// Advantages of React Query
+// Manages Seriver states
+// catching data
+// retrying if first call is failed (3 times by default)
+// give loading,error and pending states
+// cleanup code if using react query
 
 function App() {
   return (
@@ -6,6 +15,7 @@ function App() {
       <h1>The Awesome React query1</h1>
       <RQ1 />
       <h1>The Awesome React query2</h1>
+      <RQ2/>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Products from './components/RQ1/Products.jsx';
 import Product from './components/RQ1/Product.jsx';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Paginated from './components/RQ2/paginated.jsx';
+import Parallel from './components/RQ2/Parallel.jsx';
+import Optimistic from './components/RQ2/Optimistic.jsx';
+import Dependant from './components/RQ2/Dependent.jsx';
 
 const router = createBrowserRouter([
   {
@@ -19,10 +24,37 @@ const router = createBrowserRouter([
     path: '/products/:productId',
     element: <Product />,
   },
+
+  {
+    path: 'paginated',
+    element: <Paginated />,
+  },
+  {
+    path: 'parallel',
+    element: <Parallel />,
+  },
+  {
+    path: 'optimistic',
+    element: <Optimistic />,
+  },
+  {
+    path: 'dependant',
+    element: <Dependant />,
+  },
 ]);
 
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 10000,
+//     },
+//   },
+// });
+export const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
